d48e17a47c4cd39199824e365c319d51189e14fb
v.osipov <v.osipov@a1qa.com>
2022-06-21 16:22:30 +0300

    Add stub for task

---
+++ RestApiDatabase/RestApiDatabase/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RestApiDatabase
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
